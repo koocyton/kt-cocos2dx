2fb1d5277f8d2b9f883e4be3ac39820a78571eaa
koocyton <koocyton@gmail.com>
2014-06-21 13:06:15 +0800

    fix error

--- quick-cocos2dx-shell/scripts/UpgradeScene.lua
+++ quick-cocos2dx-shell/scripts/UpgradeScene.lua
@@ -13,26 +13,6 @@ function UpgradeScene:ctor()
 	-- display.newSprite("update_bg.jpg"):pos(display.cx, display.cy):addTo(self)
 end
 
-function UpgradeScene:updateBegin()
-
-	--[[ 启用触摸
-	self:setTouchEnabled(true)
-	-- 设置触摸模式
-	-- self:setTouchMode(cc.TOUCH_MODE_ALL_AT_ONCE) -- 多点
-	self:setTouchMode(cc.TOUCH_MODE_ONE_BY_ONE) -- 单点（默认模式）
-	-- 添加触摸事件处理函数
-	self:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
-	print(111)
-	-- 返回 true 表示要响应该触摸事件，并继续接收该触摸事件的状态变化
-	return true
-	end)
-
-	ui.newTTFLabel({text = "Touch To Star", size = 30, align = ui.TEXT_ALIGN_CENTER})
-		:pos(display.cx, display.cy / 3)
-		:addTo(self)
-	--]]
-end
-
 -- begin upgrade
 function UpgradeScene:onUpgradeBegin()
 	print("onUpgradeBegin")
@@ -63,8 +43,6 @@ end
 
 -- on exit this scene
 function UpgradeScene:onExit()
-	-- 关闭事件
-	--self:removeAllEventListeners();
 end
 
 -- return scene
