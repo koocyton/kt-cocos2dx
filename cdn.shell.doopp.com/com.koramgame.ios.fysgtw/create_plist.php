b8656ea6ed15b93fb9879c7714ff105720e2e8af
koocyton <koocyton@gmail.com>
2014-06-24 18:49:40 +0800

    add upgrade create php

---
+++ cdn.shell.doopp.com/com.koramgame.ios.fysgtw/create_plist.php
@@ -0,0 +1,58 @@
+<?php //-*-coding:utf-8;mode:php-mode;-*-
+
+$package_name = array(	"com.koramgame.ios.fysgtw",
+						"com.koramgame.android.fysgtw" );
+
+$update_source = array(	"http://cdn.shell.doopp.com/com.koramgame.ios.fysgtw/",
+						"http://cdn.shell.doopp.com/com.koramgame.android.fysgtw/" );
+
+$upgrade_version = "1.11";
+
+function getUpgradePlist($root_dir, $now_dir)
+{
+    $ret = "";
+    $dir_handle = @opendir($now_dir);
+    while (($file = readdir($dir_handle)) !== false)
+    {
+        if($file=='.' || $file=='..' || $file=='create_plist.php' || $file=='last_version.plist') {
+            continue;
+        }
+        $file = $now_dir . DIRECTORY_SEPARATOR . $file;
+        if (filetype($file)=="dir") {
+            $ret .= getUpgradePlist($root_dir, $file);
+        }
+        else {
+            $start = 1 + strlen($root_dir);
+            $ret .= "\n" . str_replace("\\", "/", substr($file, $start) ." ". md5_file($file) ." ". filesize($file));
+        }
+    }
+    closedir($dir_handle);
+    return $ret;
+}
+
+/***********************************************************/
+
+$dir_name = dirname(__FILE__);
+
+$upgrade_plist = "@package_name";
+$first_loop = true;
+foreach($package_name as $name) {
+    $upgrade_plist .= $first_loop ? " ".$name : ", " . $name;
+    $first_loop = false;
+}
+
+$upgrade_plist .= "\n@update_source";
+$first_loop = true;
+foreach($update_source as $source) {
+    $upgrade_plist .= $first_loop ? " ".$source : ", " . $source;
+    $first_loop = false;
+}
+
+$upgrade_plist .= "\n@version 1.11\n";
+
+$upgrade_plist .= getUpgradePlist($dir_name, $dir_name);
+
+file_put_contents($dir_name . DIRECTORY_SEPARATOR . "last_version.plist", $upgrade_plist);
+
+
+
---
+++ quick-cocos2dx-shell/res/WelcomeScene.lua
@@ -0,0 +1,40 @@
+-- scene class
+local WelcomeScene = class("WelcomeScene", function()
+	return display.newScene("WelcomeScene")
+end)
+
+-- scene init
+function WelcomeScene:ctor()
+	display.newSprite("welcome_bg.jpg"):pos(display.cx, display.cy):addTo(self)
+end
+
+function WelcomeScene:showTouch()
+
+	self:setTouchEnabled(true)
+	-- 设置触摸模式
+	-- self:setTouchMode(cc.TOUCH_MODE_ALL_AT_ONCE) -- 多点
+	self:setTouchMode(cc.TOUCH_MODE_ONE_BY_ONE) -- 单点（默认模式）
+	-- 添加触摸事件处理函数
+	self:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
+		print(111)
+		-- 返回 true 表示要响应该触摸事件，并继续接收该触摸事件的状态变化
+		return true
+	end)
+
+	ui.newTTFLabel({text="Touch me",size=30,align=ui.TEXT_ALIGN_CENTER}):pos(display.cx, display.cy / 3):addTo(self)
+end
+
+-- on enter this scene
+function WelcomeScene:onEnter()
+	print(" \n >>>>>>>>>>>>>>>>>  WelcomeScene:onEnter  >>>>>>>>>>>>>>>>>>>> \n ")
+	self:showTouch()
+end
+
+-- on exit this scene
+function WelcomeScene:onExit()
+	-- 关闭事件
+	self:removeAllEventListeners();
+end
+
+-- return scene
+return WelcomeScene
