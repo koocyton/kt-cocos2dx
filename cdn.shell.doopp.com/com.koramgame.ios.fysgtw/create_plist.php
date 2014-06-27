4780b96aa8b3a498e8ac60bbf954eee58f6ecddb
koocyton <koocyton@gmail.com>
2014-06-27 14:26:43 +0800

    改为按版本管理

--- cdn.shell.doopp.com/com.koramgame.ios.fysgtw/create_plist.php
+++ cdn.shell.doopp.com/com.koramgame.ios.fysgtw/create_plist.php
@@ -3,10 +3,8 @@
 $package_name = array(	"com.koramgame.ios.fysgtw",
 						"com.koramgame.android.fysgtw" );
 
-$update_source = array(	"http://cdn.shell.doopp.com/com.koramgame.ios.fysgtw/",
-						"http://cdn.shell.doopp.com/com.koramgame.android.fysgtw/" );
-
-$upgrade_version = "1.11";
+$update_source = array(	"http://static1.kunlun.com/test-game1/com.koramgame.ios.fysgtw/",
+						"http://static1.kunlun.com/test-game1/com.koramgame.android.fysgtw/" );
 
 function getUpgradePlist($root_dir, $now_dir)
 {
@@ -32,27 +30,35 @@ function getUpgradePlist($root_dir, $now_dir)
 
 /***********************************************************/
 
-$dir_name = dirname(__FILE__);
+$root_dir = dirname(__FILE__);
 
-$upgrade_plist = "@package_name";
+$package_name_plist = "@package_name";
 $first_loop = true;
 foreach($package_name as $name) {
-    $upgrade_plist .= $first_loop ? " ".$name : ", " . $name;
+    $package_name_plist .= $first_loop ? " ".$name : ", " . $name;
     $first_loop = false;
 }
 
-$upgrade_plist .= "\n@update_source";
-$first_loop = true;
-foreach($update_source as $source) {
-    $upgrade_plist .= $first_loop ? " ".$source : ", " . $source;
-    $first_loop = false;
+$compare_version = "";
+$root_dir_handle = @opendir($root_dir);
+while (($version = readdir($root_dir_handle)) !== false)
+{
+	if($version{0}=='.' || filetype($version)!="dir") {
+		continue;
+	}
+	$compare_version = ($compare_version=="") ? $version : version_compare($compare_version, $version)==-1 
+											  ? $version : $compare_version;
+	$upgrade_plist = $package_name_plist . "\n@update_source";
+	$first_loop = true;
+	foreach($update_source as $source) {
+		$upgrade_plist .= $first_loop ? " ".$source . $version : ", " . $source . $version;
+		$first_loop = false;
+	}
+	$upgrade_plist .= "\n@version " . $version . "\n";
+	$version_dir = $root_dir . DIRECTORY_SEPARATOR . $version;
+	$upgrade_plist .= getUpgradePlist($version_dir, $version_dir);
+	file_put_contents($root_dir . DIRECTORY_SEPARATOR . $version . ".plist", $upgrade_plist);
 }
+copy($root_dir.DIRECTORY_SEPARATOR.$compare_version.".plist", $root_dir.DIRECTORY_SEPARATOR."last_version.plist");
 
-$upgrade_plist .= "\n@version 1.11\n";
-
-$upgrade_plist .= getUpgradePlist($dir_name, $dir_name);
-
-file_put_contents($dir_name . DIRECTORY_SEPARATOR . "last_version.plist", $upgrade_plist);
-
-
-
+closedir($root_dir_handle);
